b16182ac3697e73350b88edd673a21bb0d76a44c
kiru <kiru@tutanota.com>
2019-09-10 19:55:02 +0200

    Rewrite correctly WebSocket connection.

--- index.js
+++ index.js
@@ -1,97 +1,123 @@
 const { Plugin } = require('powercord/entities');
 const { waitFor } = require('powercord/util');
 
+let heartbeatInterval;
+let data;
+let ws;
+
+function heartbeat (interval) {
+  heartbeatInterval = setInterval(() => {
+    ws.send(JSON.stringify({ op: 9 }));
+  }, interval);
+}
+
+function connect () {
+  console.log('Connecting to WebSocket');
+  ws = new WebSocket('wss://listen.moe/gateway_v2');
+
+  ws.onopen = () => {
+    clearInterval(heartbeatInterval);
+    heartbeatInterval = null;
+  };
+
+  ws.onmessage = message => {
+    if (!message.data.length) {
+      return;
+    }
+    let response;
+    try {
+      response = JSON.parse(message.data);
+    } catch (error) {
+      return;
+    }
+    switch (response.op) {
+      case 0:
+        ws.send(JSON.stringify({ op: 9 }));
+        console.log('Received OP 0 | Sent OP 9!');
+        heartbeat(response.d.heartbeat);
+        break;
+      case 1:
+        console.log('Received OP 1');
+        if (response.t !== 'TRACK_UPDATE' && response.t !== 'TRACK_UPDATE_REQUEST' && response.t !== 'QUEUE_UPDATE' && response.t !== 'NOTIFICATION') {
+          break;
+        }
+        data = response.d;
+        break;
+      default:
+        break;
+    }
+  };
+
+  ws.onclose = error => {
+    clearInterval(heartbeatInterval);
+    heartbeatInterval = null;
+    if (ws) {
+      ws.close();
+      ws = null;
+    }
+    setTimeout(() => connect(), 5000);
+  };
+}
+
+
 module.exports = class listenmoe extends Plugin {
-	async startPlugin () {	
-		await waitFor(`.container-3baos1`) // Waits Discord to load before autostart
-			this.audio = document.createElement('audio')
-			this.audio.autoplay = true
-			this.audio.src = 'https://listen.moe/opus' // Feel free to change it to kpop if you want.
-			this.audio.volume = 1;
-			this.ws = new WebSocket('wss://listen.moe/gateway_v2');
-			this.data = null;
-			this.heartbeatInterval = null;
-			ws.onopen = () => {
-				clearInterval(this.heartbeatInterval);
-				console.log('Opened WebSocket!')
-				this.heartbeatInterval = null;
-			};
+  async startPlugin () {
+    await connect();
 
-			ws.onmessage = message => {
-				if (!message.data.length) return;
-				let response;
-				try {
-					response = JSON.parse(message.data);
-				} catch (error) {
-					return;
-				}
-				switch (response.op) {
-					case 0:
-						ws.send(JSON.stringify({ op: 9 }));
-						console.log('Received OP 0 | Sent OP 9');
-						heartbeat(response.d.heartbeat);
-						break;
-					case 1:
-						if (response.t !== 'TRACK_UPDATE' && response.t !== 'TRACK_UPDATE_REQUEST' && response.t !== 'QUEUE_UPDATE' && response.t !== 'NOTIFICATION') break;
-						this.data = response.d;
-						break;
-					default:
-						break;
-				}
-			};
-		
-		
-		this.registerCommand(
-			'lvolume',
-			[],
-			'Change the volume.',
-			'{c} [0-100]',
-			(args) => {
-				if (args > 100 || args < 0) return {
-					send: false,
-					result: 'You need to specify a number between 0 and 100.'
-				}
-				this.audio.volume = args;
-			}
-		)
+    await waitFor('.container-3baos1'); // Waits Discord to load before autostart
 
-		this.registerCommand(
-			'lpause',
-			[],
-			'Pause the music.',
-			'{c}',
-			() => {
-				this.audio.pause()
-			}
-		)
+    this.heartbeatInterval = heartbeatInterval;
+    this.audio = document.createElement('audio');
+    this.audio.autoplay = true;
+    this.audio.src = 'https://listen.moe/stream'; // Feel free to change it to kpop if you want.
+    this.audio.volume = 0.5;
 
-		this.registerCommand(
-			'lresume',
-			[],
-			'Resume the music.',
-			'{c}',
-			() => {
-				this.audio.resume()
-			}
-		)
 
-		this.registerCommand(
-			'lnp',
-			[],
-			'See what\'s now playing',
-			'{c}',
-			() => {
-				return {
-					send: false,
-					result: `${this.data.song.title} by ${this.data.artists[0].name}`
-				}
-			}
-		)
-	}
-}
+    this.registerCommand(
+      'lvolume',
+      [],
+      'Change the volume.',
+      '{c} [0-1]',
+      (args) => {
+        if (args > 1 || args < 0) {
+          return {
+            send: false,
+            result: 'You need to specify a number between 0 and 1.'
+          };
+        }
+        this.audio.volume = args;
+      }
+    );
 
-function heartbeat(interval) {
-	heartbeatInterval = setInterval(() => {
-		ws.send(JSON.stringify({ op: 9 }));
-	}, interval);
-}
+    this.registerCommand(
+      'lpause',
+      [],
+      'Pause the music.',
+      '{c}',
+      () => {
+        this.audio.pause();
+      }
+    );
+
+    this.registerCommand(
+      'lresume',
+      [],
+      'Resume the music.',
+      '{c}',
+      () => {
+        this.audio.play();
+      }
+    );
+
+    this.registerCommand(
+      'lnp',
+      [],
+      'See what\'s now playing',
+      '{c}',
+      () => ({
+        	send: false,
+        result: `${data.song.title} by ${data.song.artists[0].nameRomaji || data.song.artists[0].name}`
+      })
+    );
+  }
+};
